b5211338fb8412ec1b7ca72ef05fe3c3cd93d601
EmuLock <kulthumeliase@gmail.com>
2019-05-25 15:48:04 +0800

    finsihed javascript

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function calculateDaysNeeded() {
     const answer = totalPages/inputValue 
     document.getElementById("results").innerHTML = answer
 }
+
+function calculatePages() {
+    const totalPages = 604
+    const inputValue = document.getElementById("numberOfDays").value
+    const answer = totalPages/inputValue
+    document.getElementById("endResults").innerHTML = answer
+}
