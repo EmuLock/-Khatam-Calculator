57537e973d19631c3ed4757798448be5a2fcf8b4
EmuLock <kulthumeliase@gmail.com>
2019-05-25 15:06:49 +0800

    completed first calculation for khatam
    days needed completed

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 function calculateDaysNeeded() {
-    
+    const totalPages = 604
+    const inputValue = document.getElementById("number").value
+    const answer = totalPages/inputValue 
+    document.getElementById("results").innerHTML = answer
 }
